b6b8f2f209bae24c1dd37d54d75c6a1c2e51d145
Gabriel Nascimento <barbosanascimentogabriel@gmail.com>
2017-08-08 13:31:11 -0300

    alterar função

--- code/js/views/usuarios.component.js
+++ code/js/views/usuarios.component.js
@@ -1,7 +1,7 @@
 Vue.component('usuarios-view', {
     template: `
 <div id="usuarios-view">
-    <div id="seachbar" class="card-view mdl-card mdl-shadow--2dp">
+    <div id="seachbar" class="card-view mdl-card mdl-shadow--2dp" style="overflow: visible">
         <div class="mdl-card__title">
             <div class="mdl-textfield mdl-js-textfield" id="search-input">
                 <input class="mdl-textfield__input" type="text" id="search" v-model="searchText">
@@ -13,20 +13,46 @@ Vue.component('usuarios-view', {
         </div>
         <div class="mdl-card__actions mdl-card--border">
             <ul class="usuarios-list mdl-list">
-                <li class="mdl-list__item">
+                <li class="mdl-list__item" style="padding: 8px 16px">
                     <span class="mdl-list__item-primary-content">
                         Usuário A
                     </span>
+                    <button id="mudar-funcao-0" class="mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab mdl-color--indigo-500 mdl-color-text--white">
+                        <i class="material-icons">assignment</i>
+                    </button>
+                    <ul class="mdl-menu mdl-menu--bottom-right mdl-js-menu mdl-js-ripple-effect" for="mudar-funcao-0">
+                        <li class="mdl-menu__item">Função A</li>
+                        <li class="mdl-menu__item">Função B</li>
+                        <li class="mdl-menu__item">Função C</li>
+                    </ul>
                 </li>
-                <li class="mdl-list__item">
+                <hr style="border-color: #e5e5e5; margin: 0" />
+                <li class="mdl-list__item" style="padding: 8px 16px">
                     <span class="mdl-list__item-primary-content">
                         Usuário B
                     </span>
+                    <button id="mudar-funcao-1" class="mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab mdl-color--indigo-500 mdl-color-text--white">
+                        <i class="material-icons">assignment</i>
+                    </button>
+                    <ul class="mdl-menu mdl-menu--bottom-right mdl-js-menu mdl-js-ripple-effect" for="mudar-funcao-1">
+                        <li class="mdl-menu__item">Função A</li>
+                        <li class="mdl-menu__item">Função B</li>
+                        <li class="mdl-menu__item">Função C</li>
+                    </ul>
                 </li>
-                <li class="mdl-list__item">
+                <hr style="border-color: #e5e5e5; margin: 0" />
+                <li class="mdl-list__item" style="padding: 8px 16px">
                     <span class="mdl-list__item-primary-content">
                         Usuário C
                     </span>
+                    <button id="mudar-funcao-2" class="mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab mdl-color--indigo-500 mdl-color-text--white">
+                        <i class="material-icons">assignment</i>
+                    </button>
+                    <ul class="mdl-menu mdl-menu--bottom-right mdl-js-menu mdl-js-ripple-effect" for="mudar-funcao-2">
+                        <li class="mdl-menu__item">Função A</li>
+                        <li class="mdl-menu__item">Função B</li>
+                        <li class="mdl-menu__item">Função C</li>
+                    </ul>
                 </li>
             </ul>
         </div>
